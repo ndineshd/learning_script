9f3fb44d0dd86ba6460adf6c26e3dc8387476072
ndineshd <ndineshb@northladder.com>
2023-09-04 15:25:00 +0530

    feat: Add input output processing

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-//input
-var username = prompt("give me username")
- //processing
-var welcomemwssage = "this script works!! " + username
-//output
-alert(welcomemwssage)
+var btntranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-input");
+var outputdiv=document.querySelector("#output");
+ 
+
+ function clickHandler() {
+    outputdiv.innerText = "translated: " +textInput.value;
+ };
+
+ btntranslate.addEventListener("click", clickHandler)

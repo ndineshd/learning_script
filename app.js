77866bfe1c00a9154087283c3a030213209d0d4c
ndineshd <ndineshb@northladder.com>
2023-09-06 17:28:36 +0530

    feat: Add api

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ btntranslate.addEventListener(function clickEventHandler() {
    console.log('Clicked')
 }
 */
-
 var btnTranslate = document.querySelector("#input-btn");
 var textInput = document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
  
 //var serverURL = "https://predictor.yandex.net/suggest.json/complete"
 //var serverURL = "https://api.funtranslations.com/translate/minion.json"
-var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+//var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
 
 function getTranslationURL(input){
    return  serverURL + "?" + "text=" + input

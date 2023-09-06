58803f9c88bf4fdcd3159df54883c96bf8de1758
ndineshd <ndineshb@northladder.com>
2023-09-06 12:19:32 +0530

    feat: Add functionality to homepage

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ btntranslate.addEventListener(function clickEventHandler() {
 }
 */
 
-var btntranslate = document.querySelector("#btn-translate");
+var inputBtn = document.querySelector("#input-btn");
 var textInput = document.querySelector("#txt-input");
 var outputdiv=document.querySelector("#output");
  
+//console.log(outputdiv)
 
  function clickHandler() {
-    outputdiv.innerText = "translated: " +textInput.value;
+    outputdiv.innerText = textInput.value;
+    
  };
 
- btntranslate.addEventListener("click", clickEventHandler)
+ inputBtn.addEventListener("click", clickHandler);
